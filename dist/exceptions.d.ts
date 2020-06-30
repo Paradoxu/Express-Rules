fd8b54b15f6d3257cfc532780f8e48feaa57b59f
Leandro Cardoso <20-l.cardoso@users.noreply.gitlab.3logic.it>
2020-06-30 16:07:48 +0200

    Removed node modules

--- dist/exceptions.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class UnauthorizedReadOperation extends Error {
-    constructor();
-}
-export declare class UnauthorizedWriteOperation extends Error {
-    constructor();
-}
-export declare class UnauthorizedOperation extends Error {
-    constructor();
-}
